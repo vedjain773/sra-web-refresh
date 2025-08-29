63afa6a371522b8b9787c878ffeb413941c7c325
Ved Jain <vedjain773@gmail.com>
2025-08-30 01:20:19 +0530

    Increased row size to 5 elements (Fixes #6)

--- components/OurReach/OurReach.js
+++ components/OurReach/OurReach.js
@@ -3,17 +3,16 @@ import styles from './HomeReach.module.scss';
 import { useState, useEffect } from 'react';
 
 const OurReach = () => {
-
   let groups = [];
 
-  for (let i = 0; i < AlumniReachImgNames.length; i += 4) {
+  for (let i = 0; i < AlumniReachImgNames.length; i += 5) {
     let group = [];
 
-    for (let j = i; j < i + 4; j++) {
+    for (let j = i; j < i + 5; j++) {
       if (j < AlumniReachImgNames.length) {
         const obj = {
           name: AlumniReachImgNames[j],
-          link: AlumniReachLinks[j]
+          link: AlumniReachLinks[j],
         };
 
         group.push(obj);
@@ -33,10 +32,10 @@ const OurReach = () => {
       setFade(false);
 
       setTimeout(() => {
-        setCurrentIndex(prevIndex => (prevIndex + 1) % groups.length);
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % groups.length);
         setFade(true);
       }, 1000);
-    }, 5000)
+    }, 5000);
 
     return () => clearInterval(interval);
   }, [groups.length]);
@@ -46,7 +45,11 @@ const OurReach = () => {
       <div className={styles.reach}>
         <h1>University Reach</h1>
 
-        <div className={`${styles.reachGroup} ${fade ? styles.fade : styles.noFade}`}>
+        <div
+          className={`${styles.reachGroup} ${
+            fade ? styles.fade : styles.noFade
+          }`}
+        >
           {groups[currentIndex].map((obj, id) => {
             return (
               <a href={`${obj.link}`}>
@@ -54,13 +57,12 @@ const OurReach = () => {
                   src={`/static/images/reach/${obj.name}`}
                   alt=''
                   className={styles.reachImg}
-                  key={`alumni_reach_${currentIndex*4 + id}`}
+                  key={`alumni_reach_${currentIndex * 4 + id}`}
                 ></img>
               </a>
             );
-          })} 
+          })}
         </div>
-
       </div>
     </>
   );
