f0102a982b3934a8a442220880b31a521f4d1aa1
Ved Jain <vedjain773@gmail.com>
2025-08-30 00:40:31 +0530

    Simplified the academic reach section (Fixes #6)

--- components/OurReach/OurReach.js
+++ components/OurReach/OurReach.js
@@ -1,25 +1,66 @@
 import { AlumniReachImgNames, AlumniReachLinks } from '../../data';
 import styles from './HomeReach.module.scss';
+import { useState, useEffect } from 'react';
 
 const OurReach = () => {
+
+  let groups = [];
+
+  for (let i = 0; i < AlumniReachImgNames.length; i += 4) {
+    let group = [];
+
+    for (let j = i; j < i + 4; j++) {
+      if (j < AlumniReachImgNames.length) {
+        const obj = {
+          name: AlumniReachImgNames[j],
+          link: AlumniReachLinks[j]
+        };
+
+        group.push(obj);
+      } else {
+        break;
+      }
+    }
+
+    groups.push(group);
+  }
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [fade, setFade] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFade(false);
+
+      setTimeout(() => {
+        setCurrentIndex(prevIndex => (prevIndex + 1) % groups.length);
+        setFade(true);
+      }, 1000);
+    }, 5000)
+
+    return () => clearInterval(interval);
+  }, [groups.length]);
+
   return (
     <>
       <div className={styles.reach}>
         <h1>University Reach</h1>
-        <div className={styles.reachGroup}>
-          {AlumniReachImgNames.map((name, idx) => {
+
+        <div className={`${styles.reachGroup} ${fade ? styles.fade : styles.noFade}`}>
+          {groups[currentIndex].map((obj, id) => {
             return (
-              <a href={`${AlumniReachLinks[idx]}`}>
+              <a href={`${obj.link}`}>
                 <img
-                  src={`/static/images/reach/${name}`}
+                  src={`/static/images/reach/${obj.name}`}
                   alt=''
                   className={styles.reachImg}
-                  key={`alumni_reach_${idx}`}
+                  key={`alumni_reach_${currentIndex*4 + id}`}
                 ></img>
               </a>
             );
-          })}
+          })} 
         </div>
+
       </div>
     </>
   );
